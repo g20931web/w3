39084fbc5be739e1f691899da3bcc8b77d33da90
g20931web <95006371+g20931web@users.noreply.github.com>
2021-12-02 11:37:38 +0900

    動作確認

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 // Image switcher code
 
 let myImage = document.querySelector('img');
+//document は組み込みのオブジェクト
+//ブラウザが表示しているページ、DOMとも呼ぶ。Document　Object Modelの略
+//querySelectorはページの一部（HTML要素）を選ぶ関数
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
@@ -18,15 +21,19 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  //promptは組み込みの関数。prompt （pop-up） windowを表示してユーザ入力をさせる
+  
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    //入力データを、特別ばな場所（変数ではないファイルなどに相当）に保存
+    myHeading.innerHTML = 'こんにちは, ' + myName;
   }
 }
 
 if(!localStorage.getItem('name')) {
+  //以前に特別な場所（ファイルなど）保存していた名前があれば、それを使う
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
